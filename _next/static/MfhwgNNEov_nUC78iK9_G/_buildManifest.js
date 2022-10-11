b776e2614cac0f356c6f5d26fca46cab363254e4
daniyal966 <danial.zulfiqar@developers.studio>
2022-10-11 15:33:26 +0500

    Updates

---
+++ _next/static/MfhwgNNEov_nUC78iK9_G/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-27b28b1ece318d02d382.js"],"/_error":["static/chunks/pages/_error-f1fa093a510022f0e2e9.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
